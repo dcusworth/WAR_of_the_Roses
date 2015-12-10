664f950e4908a1aac8767497f42073acd42e68de
Malcolm Mason Rodriguez <malcolmjmr@gmail.com>
2015-12-10 08:56:51 -0500

    additional stats notebook

---
+++ helpers.py
@@ -0,0 +1,182 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, auc
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# Generic classification and optimization functions from last lab
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# clf - original classifier
+# parameters - grid to search over
+# X - usually your training X matrix
+# y - usually your training y 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+def cv_optimize(clf, parameters, X, y, n_jobs=1, n_folds=5, score_func=None):
+    if score_func:
+        gs = GridSearchCV(clf, param_grid=parameters, cv=n_folds, n_jobs=n_jobs, scoring=score_func)
+    else:
+        gs = GridSearchCV(clf, param_grid=parameters, n_jobs=n_jobs, cv=n_folds)
+    gs.fit(X, y)
+    print "BEST", gs.best_params_, gs.best_score_, gs.grid_scores_
+    best = gs.best_estimator_
+    return best
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# Important parameters
+# indf - Input dataframe
+# featurenames - vector of names of predictors
+# targetname - name of column you want to predict (e.g. 0 or 1, 'M' or 'F', 
+#              'yes' or 'no')
+# target1val - particular value you want to have as a 1 in the target
+# mask - boolean vector indicating test set (~mask is training set)
+# reuse_split - dictionary that contains traning and testing dataframes 
+#              (we'll use this to test different classifiers on the same 
+#              test-train splits)
+# score_func - we've used the accuracy as a way of scoring algorithms but 
+#              this can be more general later on
+# n_folds - Number of folds for cross validation ()
+# n_jobs - used for parallelization
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+def do_classify(clf, parameters, indf, featurenames, targetname, target1val, mask=None, reuse_split=None, score_func=None, n_folds=5, n_jobs=1):
+    subdf=indf[featurenames]
+    X=subdf.values
+    y=(indf[targetname].values==target1val)*1
+    if mask !=None:
+        print "using mask"
+        Xtrain, Xtest, ytrain, ytest = X[mask], X[~mask], y[mask], y[~mask]
+    if reuse_split !=None:
+        print "using reuse split"
+        Xtrain, Xtest, ytrain, ytest = reuse_split['Xtrain'], reuse_split['Xtest'], reuse_split['ytrain'], reuse_split['ytest']
+    if parameters:
+        clf = cv_optimize(clf, parameters, Xtrain, ytrain, n_jobs=n_jobs, n_folds=n_folds, score_func=score_func)
+    clf=clf.fit(Xtrain, ytrain)
+    training_accuracy = clf.score(Xtrain, ytrain)
+    test_accuracy = clf.score(Xtest, ytest)
+    print "############# based on standard predict ################"
+    print "Accuracy on training data: %0.2f" % (training_accuracy)
+    print "Accuracy on test data:     %0.2f" % (test_accuracy)
+    print confusion_matrix(ytest, clf.predict(Xtest))
+    print "########################################################"
+    return clf, Xtrain, ytrain, Xtest, ytest
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# Plot tree containing only two covariates
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+from matplotlib.colors import ListedColormap
+# cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_light = ListedColormap(['#FFAAAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+
+def plot_2tree(ax, Xtr, Xte, ytr, yte, clf, plot_train = True, plot_test = True, lab = ['Feature 1', 'Feature 2'], mesh=True, colorscale=cmap_light, cdiscrete=cmap_bold, alpha=0.3, psize=10, zfunc=False):
+    # Create a meshgrid as our test data
+    plt.figure(figsize=(15,10))
+    plot_step= 0.05
+    xmin, xmax= Xtr[:,0].min(), Xtr[:,0].max()
+    ymin, ymax= Xtr[:,1].min(), Xtr[:,1].max()
+    xx, yy = np.meshgrid(np.arange(xmin, xmax, plot_step), np.arange(ymin, ymax, plot_step) )
+
+    # Re-cast every coordinate in the meshgrid as a 2D point
+    Xplot= np.c_[xx.ravel(), yy.ravel()]
+
+
+    # Predict the class
+    Z = clfTree1.predict( Xplot )
+
+    # Re-shape the results
+    Z= Z.reshape( xx.shape )
+    cs = plt.contourf(xx, yy, Z, cmap= cmap_light, alpha=0.3)
+  
+    # Overlay training samples
+    if (plot_train == True):
+        plt.scatter(Xtr[:, 0], Xtr[:, 1], c=ytr-1, cmap=cmap_bold, alpha=alpha,edgecolor="k") 
+    # and testing points
+    if (plot_test == True):
+        plt.scatter(Xte[:, 0], Xte[:, 1], c=yte-1, cmap=cmap_bold, alpha=alpha, marker="s")
+
+    plt.xlabel(lab[0])
+    plt.ylabel(lab[1])
+    plt.title("Boundary for decision tree classifier",fontsize=7.5)
+
+from matplotlib.colors import ListedColormap
+# cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_light = ListedColormap(['#FFAAAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+
+def plot_2tree(ax, Xtr, Xte, ytr, yte, clf, plot_train = True, plot_test = True, lab = ['Feature 1', 'Feature 2'], mesh=True, colorscale=cmap_light, cdiscrete=cmap_bold, alpha=0.3, psize=10, zfunc=False):
+    # Create a meshgrid as our test data
+    plt.figure(figsize=(15,10))
+    plot_step= 0.05
+    xmin, xmax= Xtr[:,0].min(), Xtr[:,0].max()
+    ymin, ymax= Xtr[:,1].min(), Xtr[:,1].max()
+    xx, yy = np.meshgrid(np.arange(xmin, xmax, plot_step), np.arange(ymin, ymax, plot_step) )
+
+    # Re-cast every coordinate in the meshgrid as a 2D point
+    Xplot= np.c_[xx.ravel(), yy.ravel()]
+
+
+    # Predict the class
+    Z = clf.predict( Xplot )
+
+    # Re-shape the results
+    Z= Z.reshape( xx.shape )
+    cs = plt.contourf(xx, yy, Z, cmap= cmap_light, alpha=0.3)
+  
+    # Overlay training samples
+    if (plot_train == True):
+        plt.scatter(Xtr[:, 0], Xtr[:, 1], c=ytr-1, cmap=cmap_bold, alpha=alpha,edgecolor="k") 
+    # and testing points
+    if (plot_test == True):
+        plt.scatter(Xte[:, 0], Xte[:, 1], c=yte-1, cmap=cmap_bold, alpha=alpha, marker="s")
+
+    plt.xlabel(lab[0])
+    plt.ylabel(lab[1])
+    plt.title("Boundary for decision tree classifier",fontsize=7.5)
+
+def plot_decision_surface(clf, X_train, Y_train):
+    plot_step=0.1
+    
+    if X_train.shape[1] != 2:
+        raise ValueError("X_train should have exactly 2 columnns!")
+    
+    x_min, x_max = X_train[:, 0].min() - plot_step, X_train[:, 0].max() + plot_step
+    y_min, y_max = X_train[:, 1].min() - plot_step, X_train[:, 1].max() + plot_step
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                         np.arange(y_min, y_max, plot_step))
+
+    clf.fit(X_train,Y_train)
+    if hasattr(clf, 'predict_proba'):
+        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:,1]
+    else:
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])    
+    Z = Z.reshape(xx.shape)
+    cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Reds)
+    plt.scatter(X_train[:,0],X_train[:,1],c=Y_train,cmap=plt.cm.Paired)
+    plt.show()
+# change the elimination week of winners and runners up   
+def change_winner_runnerup(v):
+    if v == "Winner":
+        return 10
+    elif v == "Runner-up":
+        return 9
+    elif 'week' in v.lower():
+        for num in range(0,10):
+            if str(num) in v:
+                return num
+    else:
+        return v
